92ca90baa7012bb9b487eae7a58d41486f7b7736
kumar <kumarajay912282@gmail.com>
2026-09-11 17:58:24 +0530

    Add initial implementation of M1 class with 2D array example

---
+++ M1.java
@@ -0,0 +1,11 @@
+public class M1 {
+   public static void main(String[] arg){
+    int [][] arr = new int[3][4];
+    for(int i = 0; i< arr.length; i++){
+        for(int j = 0; j< arr[0].length; j++){
+            System.out.print(arr[i][j]);
+        }
+        System.out.println();
+    }
+   } 
+}
