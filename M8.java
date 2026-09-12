4891e4e885ac335eb3f413bc2baed2acbfc55586
kumar <kumarajay912282@gmail.com>
2026-09-12 19:41:55 +0530

    Add M8 class for zigzag printing of a 2D array

---
+++ M8.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class M8 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int[][] arr = new int[3][4];
+        System.out.println("Enter 12 numbers:");
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < arr[0].length; j++) {
+                arr[i][j] = sc.nextInt();
+            }
+        }
+        for (int i = 0; i < arr.length; i++) {
+            if (i % 2 == 0) {
+                for (int j = 0; j < arr[0].length; j++) {
+                    System.out.print(arr[i][j] + " ");
+                }
+            } else {
+                for (int j = arr[0].length - 1; j >= 0; j--) {
+                    System.out.print(arr[i][j] + " ");
+                }
+            }
+        }
+        
+        System.out.println();
+    }
+}
